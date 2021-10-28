d82fd59dedf8695a33c0d1a226e263a921b651dc
manoovau <66820526+manoovau@users.noreply.github.com>
2021-10-28 18:58:31 +0200

    update App.css, App.tsx, index.tsx, add interface.ts and delete App.test.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,139 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { Header, ItemContainer, ItemInfo } from "./component/index";
+import {
+  InputType,
+  ItemInfoType,
+  reviewMainType,
+  ApiResponseType,
+  ItemInfoChildrenType,
+} from "./interface";
+import { URL_BASE, BEARER } from "./hooks/yelp-api";
+import { Switch, Route } from "react-router-dom";
+// <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+// url image not found
+// <div>Icons made by <a href="https://www.flaticon.com/authors/payungkead" title="Payungkead">Payungkead</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+// icon rating
+// <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+const requestHeaders: HeadersInit = {
+  Authorization: BEARER,
+  Origin: "localhost",
+  withCredentials: "true",
+};
+
+const useFetchYELP = async (fetchParameter: string) => {
+  const URL = `${URL_BASE}${fetchParameter}`;
+
+  const resp = await fetch(URL, {
+    headers: requestHeaders,
+  });
+  const data = await resp.json();
+  return data;
+};
 
 function App() {
+  const DEFAULT_VALUE = null;
+  const DEFAULT_STRING = "default_string";
+  const DEFAULT_NUMBER = 0;
+  const [searchInputs, setSearchInputs] = useState<InputType>({
+    business: DEFAULT_VALUE,
+    where: DEFAULT_VALUE,
+  });
+
+  const [resultYELP, setResultYELP] = useState<ApiResponseType>({
+    businesses: [],
+    region: {},
+    total: DEFAULT_NUMBER,
+  });
+
+  const [selectedBusiness, setSelectedBusiness] = useState<ItemInfoType>({
+    id: DEFAULT_STRING,
+    name: DEFAULT_STRING,
+  });
+  const [idSelected, setIdSelected] = useState<string>();
+  const [idReviewData, setIdReviewData] = useState<string>();
+  const [reviewData, setReviewData] = useState<reviewMainType>({ total: DEFAULT_NUMBER });
+
+  const LIMIT = 20;
+  const [term, setTerm] = useState<string>(
+    `?term=${searchInputs.business}&location=${searchInputs.where}&limit=${LIMIT}`,
+  );
+  const PATH = "/businesses/search";
+
+  const [itemInfoChildren, setItemInfoChildren] = useState<ItemInfoChildrenType>({
+    selectedBusiness: { id: DEFAULT_STRING, name: DEFAULT_STRING },
+    reviewData: { total: DEFAULT_NUMBER },
+  });
+
+  useEffect(
+    () => setTerm(`?term=${searchInputs.business}&location=${searchInputs.where}&limit=${LIMIT}`),
+    [searchInputs],
+  );
+
+  useEffect(() => {
+    if (term !== `?term=${DEFAULT_VALUE}&location=${DEFAULT_VALUE}&limit=${LIMIT}`)
+      Promise.resolve(useFetchYELP(`${PATH}${term}`))
+        .then((resp) => setResultYELP(resp))
+        .catch((err) => console.error(err));
+  }, [term]);
+
+  useEffect(() => {
+    console.log(idSelected);
+    console.log(idSelected !== undefined);
+    if (idSelected !== undefined)
+      Promise.resolve(useFetchYELP(`/businesses/${idSelected}`))
+        .then((resp) => {
+          console.log("SelectBus");
+          setSelectedBusiness(resp);
+        })
+        .catch((err) => console.error(err));
+  }, [idSelected]);
+
+  useEffect(() => {
+    if (idReviewData !== undefined)
+      Promise.resolve(useFetchYELP(`/businesses/${idReviewData}/reviews`))
+        .then((resp) => setReviewData(resp))
+        .catch((err) => console.error(err));
+  }, [idReviewData]);
+
+  useEffect(() => {
+    setItemInfoChildren({ ...itemInfoChildren, selectedBusiness: selectedBusiness });
+  }, [selectedBusiness]);
+  useEffect(() => {
+    setItemInfoChildren({ ...itemInfoChildren, reviewData: reviewData });
+  }, [reviewData]);
+
+  const updateSearchInputs = (objectIn: InputType) => {
+    setSearchInputs({ ...searchInputs, business: objectIn.business, where: objectIn.where });
+    if (!searchInputs.where) {
+      (document.getElementById("where") as HTMLInputElement).placeholder =
+        "Please, fill location field.";
+    } else {
+      (document.getElementById("where") as HTMLInputElement).placeholder = "Where...";
+    }
+  };
+
+  console.log("resultYELP");
+  console.log(resultYELP);
+  console.log(selectedBusiness);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <div>
+            <Header updateSearchInputs={updateSearchInputs} />
+            <div id="result-container">
+              <ItemContainer setIdSelected={setIdSelected}>{resultYELP}</ItemContainer>
+            </div>
+          </div>
+        </Route>
+        <Route path={`/${selectedBusiness.id}`}>
+          <div>
+            <ItemInfo setIdReviewData={setIdReviewData}>{itemInfoChildren}</ItemInfo>
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/interface.ts
@@ -0,0 +1,88 @@
+export interface ItemInfoChildrenType {
+  selectedBusiness: ItemInfoType;
+  reviewData: reviewMainType;
+}
+
+export interface InputType {
+  business: string | null;
+  where: string | null;
+}
+
+export interface ApiResponseType {
+  businesses: ItemInfoType[];
+  region: Object;
+  total: number;
+}
+
+export interface ItemInfoType {
+  id: string;
+  name: string;
+  is_claimed?: boolean;
+  is_closed?: boolean;
+  photos?: string[];
+  categories?: categoriesType[];
+  phone?: string | null;
+  hours?: HoursType[];
+  location?: LocationType;
+  price?: string;
+  rating?: number;
+  review_count?: number;
+  transactions?: string[];
+  url?: string | null;
+  image_url?: string | null;
+}
+
+export interface HoursType {
+  hours_type?: string;
+  is_open_now?: boolean;
+  open: OpenType[];
+}
+
+export interface OpenType {
+  is_overnight: boolean;
+  start: string;
+  end: string;
+}
+
+export interface LocationType {
+  address1?: string | null;
+  address2?: string | null;
+  address3?: string | null;
+  city?: string | null;
+  country?: string | null;
+  cross_streets?: string | null;
+  display_address?: string[];
+  state?: string | null;
+  zip_code?: string | null;
+}
+
+export interface categoriesType {
+  alias: string;
+  title: string;
+}
+
+export interface updateSearchInputsType {
+  updateSearchInputs: (objectIn: InputType) => void;
+}
+
+export interface reviewMainType {
+  possible_languages?: string[];
+  reviews?: reviewsType[];
+  total: number;
+}
+
+export interface reviewsType {
+  id: string;
+  rating: number;
+  user: UserType;
+  text?: string;
+  time_created?: string;
+  url?: string | null;
+}
+
+export interface UserType {
+  id: string;
+  profile_url?: string | null;
+  image_url?: string | null;
+  name: string;
+}
